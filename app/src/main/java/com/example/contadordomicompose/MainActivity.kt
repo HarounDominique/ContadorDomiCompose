82dda7e55ea0f9fed909dd96b51f6c557b82bc78
HAROUN <haroundominique@gmail.com>
2023-02-18 03:19:01 +0100

    Terminada la disposición de los elementos y parte de la funcionalidad más básica. Resta crear los text field donde el usuario pueda introducir el valor a incrementar en los contadores.

--- app/src/main/java/com/example/contadordomicompose/MainActivity.kt
+++ app/src/main/java/com/example/contadordomicompose/MainActivity.kt
@@ -5,18 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.contadordomicompose.ui.theme.ContadorDomiComposeTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.Icon
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,38 +36,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    var cont1 by remember { mutableStateOf(0) }
+    var cont2 by remember { mutableStateOf(0) }
+    var contGlobal by remember { mutableStateOf(0) }
+    var incrementoCont1 by remember { mutableStateOf(1) }
+    var incrementoCont2 by remember { mutableStateOf(1) }
     Column(
         Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column(Modifier.weight(1f), verticalArrangement = Arrangement.Center) {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Button(onClick = { /*TODO*/ }) {
-                    val cont1 = 0
-                    Text("Contador 1 $cont1")
+                Button(onClick = { cont1+incrementoCont1; contGlobal+incrementoCont1 }) {
+                    Text("Contador 1")
                 }
+                Text(" $cont1")
                 Icon(Icons.Filled.Delete, contentDescription = "Papelera", Modifier.clickable { })
             }
             Row() {
-                Text("Incremento:")
+                Text("Incremento: $incrementoCont1")
+
             }
         }
         Column(Modifier.weight(1f)) {
-            Row() {
-                Button(onClick = { /*TODO*/ }) {
-                    val cont2 = 0
-                    Text("Contador 2 $cont2")
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Button(onClick = { cont2+incrementoCont2; contGlobal+incrementoCont2 }) {
+
+                    Text("Contador 2")
                 }
+                Text(" $cont2")
+                Icon(Icons.Filled.Delete, contentDescription = "Papelera")
             }
             Row() {
-                Text("Incremento:")
-                Icon(Icons.Filled.Delete, contentDescription = "Papelera")
+                Text("Incremento: $incrementoCont2")
 
             }
         }
 
-        Row(Modifier.weight(0.5f)) {
-            val contGlobal = 0
+        Row(Modifier.weight(1f)) {
             Text("Global $contGlobal")
             Icon(Icons.Filled.Delete, contentDescription = "Papelera")
         }
